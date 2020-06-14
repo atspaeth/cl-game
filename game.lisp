13cf5e635c4ca88f842cab3cd64a09508cb9cbc2
Alex Spaeth <atspaeth@ucsc.edu>
2020-06-14 12:02:16 -0700

    Switch to new SDL hardware textures.

--- game.lisp
+++ game.lisp
@@ -1,32 +1,45 @@
 (in-package :game)
 
-(require :game/util "util.lisp")
-(require :game/loader "resource-loader.lisp")
+(defparameter +screen-width+ 640)
+(defparameter +screen-height+ 480)
 
-(defparameter *screen-width* 640)
-(defparameter *screen-height* 480)
-(defparameter *screen-pixel-format* 0)
+(defvar *test-image* nil)
 
-(def-load-func :bmp (path &optional (pixel-format *screen-pixel-format*))
-  (-> path
-      sdl2:load-bmp
-      (sdl2:convert-surface-format pixel-format)))
+(defun load-texture (renderer filename)
+  (sdl2:create-texture-from-surface 
+    renderer 
+    (sdl2:load-bmp filename)))
+
+(defmacro with-window-and-renderer ((window renderer) &body body)
+  `(sdl2:with-init (:video)
+     (sdl2:with-window (,window
+                         :title "Maaaaagic"
+                         :w +screen-width+ :h +screen-height+
+                         :flags '(:shown))
+       (sdl2:with-renderer (,renderer ,window :index -1 
+                                      :flags '(:accelerated))
+         ,@body))))
 
 (defun main ()
-  (sdl2:with-init (:video)
-    (sdl2:with-window (wnd :title "SDL2 Window"
-			   :w *screen-width* :h *screen-height*
-			   :flags '(:shown))
-      (let* ((screen (sdl2:get-window-surface wnd))
-	     (*screen-pixel-format* (sdl2:surface-format-format screen)))
-	(get-resource :bg :bmp "hello_world.bmp")
-	(sdl2:with-event-loop (:method :poll)
-	  (:quit () t)
-	  (:keydown (:keysym keysym)
-		    (case (sdl2:scancode keysym)
-		      (:scancode-escape
-		       (sdl2:push-event :quit))))
-	  (:idle ()
-		 (sdl2:blit-surface (get-resource :bg :bmp) nil screen nil)
-		 (sdl2:update-window wnd)
-		 (sdl2:delay 100)))))))
+  (with-window-and-renderer (wnd renderer)
+    (sdl2:set-render-draw-color renderer #xFF #xFF #xFF #xFF)
+    (setf *test-image* 
+          (load-texture renderer "test.bmp"))
+    (sdl2:with-event-loop (:method :poll)
+      (:quit () t)
+      (:keydown (:keysym keysym)
+       (case (sdl2:scancode keysym)
+         (:scancode-escape
+          (sdl2:push-event :quit))))
+      (:idle ()
+       (livesupport:continuable
+         (livesupport:update-repl-link)
+         (draw-everything renderer)
+         (sdl2:delay 100))))))
+                            
+(defun draw-everything (renderer)
+  (sdl2:render-clear renderer)
+  (sdl2:render-copy renderer *test-image*)
+  (sdl2:render-present renderer))
+  
+
